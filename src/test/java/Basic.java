141cb61073bd03401978ba449ba04b928960352d
PranPatil05 <pranav.patil050996@gmail.com>
2023-01-16 18:29:49 +0530

    Practice 2

--- src/test/java/Basic.java
+++ src/test/java/Basic.java
@@ -4,14 +4,27 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.testng.Assert;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class Basic {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         RestAssured.baseURI="https://rahulshettyacademy.com";
+//        String response=given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
+//                .body(payload.AddPlace()).when().post("maps/api/place/add/json")
+//                .then().assertThat().statusCode(200).body("scope",equalTo("APP")).extract().response().asString();
+
+        //Giving JSON File path
+        //Body is String Type method
+        //json Coverts byte(Files.readAllBytes(Paths.get(0) ->Byte converts to String (new String)
+
+
         String response=given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
-                .body(payload.AddPlace()).when().post("maps/api/place/add/json")
+                .body(new String(Files.readAllBytes(Paths.get("C:\\Learning\\RestAssuredPractice\\src\\test\\resources\\Files\\Addbookdetails.json")))).when().post("maps/api/place/add/json")
                 .then().assertThat().statusCode(200).body("scope",equalTo("APP")).extract().response().asString();
 
         System.out.println(response);
--- src/test/java/DynamicJson.java
+++ src/test/java/DynamicJson.java
@@ -2,6 +2,7 @@ import Files.payload;
 import ReUsableMethods.reUsableMethod;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -15,7 +16,29 @@ public class DynamicJson {
 //        when().post("Library/Addbook.php").then().log().all().assertThat().statusCode(200).extract().response().asString();
 
         String response=given().header("Content-Type","application/json")
-                .body(payload.AddBook("adsfsa","6969")).
+                .body(payload.AddBook("adsfsa","6968")).
+                when().post("Library/Addbook.php").
+                then().log().all().assertThat().statusCode(200).extract().response().asString();
+
+        JsonPath js= reUsableMethod.rawToJson(response);
+        String id=js.get("ID");
+        System.out.println("ID :"+id);
+    }
+
+    @DataProvider
+    public Object[][] getData(){
+        Object[][] data=new Object[3][2];
+        data[0]=new Object[]{"ofghe","23453"};
+        data[1]=new Object[]{"retdw","435243"};
+        data[2]=new Object[]{"sfdsjd","43435"};
+        return data;
+    }
+    @Test(dataProvider = "getData")
+    public void addBook1(String isbn,String aisle ){
+        RestAssured.baseURI="https://rahulshettyacademy.com";
+
+        String response=given().header("Content-Type","application/json")
+                .body(payload.AddBook(isbn,aisle)).
                 when().post("Library/Addbook.php").
                 then().log().all().assertThat().statusCode(200).extract().response().asString();
 
